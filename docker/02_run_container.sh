6212f48408b7a71463db4a27d942b13f0deecdcd
dorr <xutao543210@163.com>
2024-11-22 20:32:40 +0800

    chore:更改容器启动方式

--- docker/02_run_container.sh
+++ docker/02_run_container.sh
@@ -1,18 +1,37 @@
 #!/bin/bash
 # Build the smbat container
-if [ "$#" -eq 1 ]; then
-    container_name=$1
-else
-    container_name=smbat
+if [ "$#" -lt 2 ]; then
+    echo "Error: This script requires at least 2 arguments."
+    exit 1
 fi
 
+image_name=$1
+container_name=$2
+
 docker run \
+    --name $container_name \
     --gpus all \
+    -e NVIDIA_DRIVER_CAPABILITIES=all \
+    -v /etc/localtime:/etc/localtime:ro \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -e DISPLAY=unix$DISPLAY \
+    -e GDK_SCALE \
+    -e GDK_DPI_SCALE \
+    --network="host" \
     --privileged \
-    --net host \
-    -e DISPLAY=:0 \
-    --name  $container_name \
+    -v /dev/bus/usb:/dev/bus/usb \
     --mount type=bind,source=/home/$USER/docker_share/workspace,target=/home/admin/workspace \
     -itd \
-    dorrproject/smbat \
+    $image_name \
     /bin/bash
+
+# docker run \
+#     --gpus all \
+#     --privileged \
+#     --net host \
+#     -e DISPLAY=:0 \
+#     --name  $container_name \
+#     --mount type=bind,source=/home/$USER/docker_share/workspace,target=/home/admin/workspace \
+#     -itd \
+#     $image_name \
+#     /bin/bash
